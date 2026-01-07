f391725a5682008c601d63c75a7db086507d044c
Piotr Dabrowski <piotrdabrowski@nudne.studio>
2026-01-07 17:49:48 +0100

    [MWP1-01] Bash CI scripts updated

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,35 +2,44 @@
 
 set -euo pipefail
 
-PRESET="$1"
+echo "Build...."
 
+PRESET="$1"
 LINER="=========="
 
-echo "($LINER) Configure ($PRESET) ($LINER)"
-cmake --preset "$PRESET"
+if [[ "$PRESET" == "all" ]]; then
+	echo "TODO"
+else
 
-echo "($LINER)  Build ($PRESET) ($LINER)"
-cmake --build --preset "$PRESET"
+	echo "($LINER) Configure ($PRESET) ($LINER)"
+	cmake --preset "$PRESET"
+	
+	echo "($LINER)  Build ($PRESET) ($LINER)"
+	cmake --build --preset "$PRESET"
+	
+	echo "($LINER)  Unit Tests on ($PRESET) ($LINER)"
+	ctest --preset "$PRESET" -V
+	
+	if [[ "$PRESET" == "asan" ]]; then
+		echo "($LINER) Sanitizers ($LINER)"
+		valgrind --error-exitcode=1 ctest --preset "$PRESET"
+	fi
+	
+	if [[ "$PRESET" == "valgrind" ]]; then
+		echo "($LINER) Valgrind Memcheck ($LINER)"
+		valgrind ../build/valgrind/nd
+	fi
+	
+	if [[ "$PRESET" == "helgrind" ]]; then
+		echo "($LINER) Helgrind ($LINER)"
+		valgrind --tool=helgrind ../build/valgrind/nd
+	fi
+	
+	if [[ "$PRESET" == "coverage" ]]; then
+		echo "($LINER) Coverage ($LINER)"
+		cmake --build --preset coverage --target coverage
+	fi
 
-echo "($LINER)  Unit Tests on ($PRESET) ($LINER)"
-ctest --preset "$PRESET" -V
-
-if [[ "$PRESET" == "asan" ]]; then
-	echo "($LINER) Sanitizers ($LINER)"
-	valgrind --error-exitcode=1 ctest --preset "$PRESET"
 fi
 
-if [[ "$PRESET" == "valgrind" ]]; then
-	echo "($LINER) Valgrind Memcheck ($LINER)"
-	valgrind ../build/valgrind/nd
-fi
-
-if [[ "$PRESET" == "helgrind" ]]; then
-	echo "($LINER) Helgrind ($LINER)"
-	valgrind --tool=helgrind ../build/valgrind/nd
-fi
-
-if [[ "$PRESET" == "coverage" ]]; then
-	echo "($LINER) Coverage ($LINER)"
-	cmake --build --preset coverage --target coverage
-fi
+echo "Done."
