15776523e00280f7eb3d516c6a14fcbc39706ffd
Piotr Dabrowski <piotrdabrowski@nudne.studio>
2026-01-07 17:13:46 +0100

    [MWP1-O2] Tensor UT - flattern data

--- nn/tests/include/nn/tensor_test.hpp
+++ nn/tests/include/nn/tensor_test.hpp
@@ -3,12 +3,21 @@
 #include <nn/tensor.hpp>
 #include <gtest/gtest.h>
 
+class TensorGetter : public nd::nn::Tensor {
+	public:
+		std::vector<float> getFlatternData() const;
+		std::vector<std::size_t> getStride() const;
+};
+
 class TensorTest : public testing::Test {
 	protected:
 		std::string scalar_exception{"Can't index scalar. Uset item()."};
 		std::vector<float> data{1.0f, 1.0f};
+		std::vector<std::vector<float>> data2d{{1,2,3},{4,5,6},{7,8,9}};
+		std::vector<std::vector<float>> data2d_invalid{{1,2,3},{4,5,6},{7,8,9,0}};
 		std::shared_ptr<nd::nn::Tensor> t0{nullptr};
 		std::shared_ptr<nd::nn::Tensor> t1{nullptr};
+		std::shared_ptr<nd::nn::Tensor> t2{nullptr};
 
 		std::size_t i{0};
 	
--- nn/tests/src/tensor_test.cpp
+++ nn/tests/src/tensor_test.cpp
@@ -1,5 +1,13 @@
 #include<nn/tensor_test.hpp>
 
+std::vector<float> TensorGetter::getFlatternData() const {
+	return m_data;
+}
+
+std::vector<std::size_t> TensorGetter::getStride() const {
+	return m_stride;
+}
+
 TEST_F(TensorTest, GetItem_On_Scalar) {
 	ASSERT_TRUE(t0);
 	EXPECT_FLOAT_EQ(t0->getItem(), 1.0f);
@@ -31,4 +39,19 @@ TEST_F(TensorTest, RuntimeError_Whet_GetItemConst_Called_On_NonScalar) {
 			const float& ct = t.getItem();
 	}, std::runtime_error);
 
+}
+
+TEST_F(TensorTest, Flattern_Tensor_Data) {
+	TensorGetter t{data2d};
+	std::vector<std::size_t> shape_proto{3, 3};
+	std::vector<std::size_t> stride_proto{3 ,1};
+	std::vector<float> data_proto{1.f, 2.f,3.f, 4.f, 5.f , 6.f, 7.f, 8.f, 9.f};
+
+	std::vector<std::size_t> shape = static_cast<std::vector<std::size_t>>(t.getShape());
+	std::vector<float>  flatData = t.getFlatternData();
+	std::vector<std::size_t> stride = t.getStride();
+
+	EXPECT_EQ(shape, shape_proto);
+	EXPECT_EQ(stride, stride_proto);
+	EXPECT_EQ(flatData, data_proto);
 }
