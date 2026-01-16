e2f05e264fe55cc83846efbebd3b7d5a94b4b486
Piotr Dabrowski <piotrdabrowski@nudne.studio>
2026-01-16 14:02:34 +0100

    [MWP1-O2-L8-UTIL] Callback queue execution update
    if callback pointer not set
    	throw exception
    else
    	if queue not empty
    		execute
    		return true
    	else
    		return false
    	endif
    endif

--- util/include/util/callback_queue.hpp
+++ util/include/util/callback_queue.hpp
@@ -23,16 +23,21 @@ namespace nd::util {
 				m_queue.push( wptr( t_object ));
 			}
 
-			void execute(TCallbackParams... t_params ){
-				std::lock_guard<std::mutex> lock( m_mutex );
+			bool execute(TCallbackParams... t_params ){
 				if(m_callback) {
-					while( !m_queue.empty() ) {
-						if( auto ptr = m_queue.front().lock() ) {
-							(ptr.get()->*m_callback)(t_params...);
+					if(!m_queue.empty()) {
+						std::lock_guard<std::mutex> lock( m_mutex );
+						while( !m_queue.empty() ) {
+							if( auto ptr = m_queue.front().lock() ) {
+								(ptr.get()->*m_callback)(t_params...);
+							}
+							m_queue.pop();
 						}
-						m_queue.pop();
+						return true;
 					}
+					return false;
 				}
+				throw std::runtime_error("Callback is null in execution queue!");
 			};
 		protected:
 			callback m_callback; 
